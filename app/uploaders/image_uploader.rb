18188f585a54eebfa1db19f83708e9216931302b
Ari Sharon <arisha84@gmail.com>
2013-11-15 16:10:23 -0500

    Trying to fix AWS issue

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,12 +7,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
    # Choose what kind of storage to use for this uploader:
-  if Rails.env.development? || Rails.env.test?
-    storage :file
-  else
+#  if Rails.env.development? || Rails.env.test?
+ #   storage :file
+  
+  #else
     storage :fog
   # storage :fog
-  end
+  #end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,23 @@
 CarrierWave.configure do |config|
   
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
+ # if Rails.env.development? || Rails.env.test?
+ 
+  
+  #  config.storage = :file
+ 
+#  else
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => 'AKIAJF54D34GBIX3S2JQ',                        # required
-      :aws_secret_access_key  => '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',                        # required
+                     
+      :aws_secret_access_key  => 'o+FAemL2yXlOh9nmnQNdQWwjmK/PZfIgTQUsyGK3',                          # required
       #:region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
-    config.fog_directory  = ENV['S3_BUCKET']                   # required
+    config.fog_directory  = 'Restaurant_app'                   # required
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-  end
+ # end
 end
